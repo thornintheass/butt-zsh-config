b7612cf8f0851d39cd3996aeaa18a397ced7bde0
Alexender Yatsenko <alexender.yatsenko@gmail.com>
2022-03-25 21:51:49 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+### [thornintheass](https://github.com/thornintheass) zsh config
+
+## Installation
+
+
+```sh
+git clone --recurse-submodules https://github.com/thornintheass/butt-zsh-config.git ~/butt-zsh-config
+```
+
+```zsh
+# add source to ~/.zshrc
+echo 'source /home/$USER/butt-zsh-config/butt-zshrc.zsh' >> ~/.zshrc
+```
---
+++ butt-zshrc.zsh
@@ -0,0 +1,24 @@
+# aliases
+USER_EDITOR=micro
+USER_HOME="/home/$USER"
+BUTT_SRC="$USER_HOME/butt-zsh-config"
+BUTT_SRC_CONF="$BUTT_SRC/.butt-zshrc"
+
+alias zshconf="$USER_EDITOR $BUTT_SRC_CONF"
+alias zshsrc="source $USER_HOME/.zshrc"
+
+# antigen src
+source "$BUTT_SRC/antigen/antigen.zsh"
+
+# antigen settings
+
+antigen use oh-my-zsh
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+
+antigen theme romkatv/powerlevel10k
+
+antigen apply
+
+
