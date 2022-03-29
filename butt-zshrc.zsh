3e77c52e8b36142d6dd4b99f6557ffed137cb77a
Alexender Yatsenko <alexender.yatsenko@gmail.com>
2022-03-29 18:15:14 +0300

    fix BUTT_SRC_CONF path

--- butt-zshrc.zsh
+++ butt-zshrc.zsh
@@ -2,7 +2,7 @@
 USER_EDITOR=micro
 USER_HOME="/home/$USER"
 BUTT_SRC="$USER_HOME/butt-zsh-config"
-BUTT_SRC_CONF="$BUTT_SRC/.butt-zshrc"
+BUTT_SRC_CONF="$BUTT_SRC/butt-zshrc.zsh"
 
 alias zshconf="$USER_EDITOR $BUTT_SRC_CONF"
 alias zshsrc="source $USER_HOME/.zshrc"
